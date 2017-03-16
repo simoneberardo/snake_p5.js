4d7081435cfcdb1be07b8b6ad518abdc4f3631a2
simoneberardo <simone.berardo991@gmail.com>
2017-03-16 07:48:44 +0100

    New features on moving snake

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ function setup(){
    this.food = new Food();
 }
 
-
 function keyPressed(){
        //moving function
        if(keyCode == 32){
@@ -25,40 +24,46 @@ function keyPressed(){
        }
       this.snake.move(keyCode);
 }
+
 function restart(){
    window.location.reload();
 }
+
+
 function draw(){
    background(0);
    this.food.show();
+
    //check if snake is in bounds
- if(this.snake.isSnakeOut(this.width, this.height)){
+/* if(this.snake.isSnakeOut(this.width, this.height)){
       noLoop();
       console.log("HAI PERSO!");
-   }
+   }*/
+
 //check if is dead
   if(this.snake.collision()){
       noLoop();
       console.log("HAI PERSO!");
    }
-
    //upedate
       this.snake.update();
    //  and show snake
       this.snake.show();
-
    //make the snake eat the food and create a new piece of snake
-   if(this.snake.eat(this.food.foodPos())){
+      if(this.snake.eat(this.food.foodPos())){
       //create a new random piec of food
-
      this.food = new Food();
      this.food.show();
       levelUp();
   }
   //level up
-
      console.log("CURRENT LVL  " +this.level);
+}
 
+function walls(){
+   for(var i = 0){
+
+   }
 }
 
 function levelUp(){
