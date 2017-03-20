2a3d2aee2dd85e3cdb1b0459b3faea98dddcd689
simoneberardo <simone.berardo991@gmail.com>
2017-03-20 23:36:46 +0100

    CSS and prevent event keydown

--- sketch.js
+++ sketch.js
@@ -37,6 +37,12 @@ function restart(){
 
 function draw(){
    background(0);
+   window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
    showScenario();
    this.food.show();
 //check if is dead
